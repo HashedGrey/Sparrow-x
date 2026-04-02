f3a02ca946228f8a66dad7f2d1e3c25703d81008
Aggrey Lelei <aggreykiplimo@gmail.com>
2026-04-02 18:26:25 +0300

    chore(deployment): add config files for Loki, Grafana, Alloy, and Prometheus

--- deployments/postgres-init/init-db.sql
+++ deployments/postgres-init/init-db.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE user_write;
 CREATE DATABASE keycloak;
--- Grant privileges if necessary, though the default 'postgres' user should have them
--- For example:
--- GRANT ALL PRIVILEGES ON DATABASE passenger_write TO postgres;
+CREATE DATABASE tweet_write;
