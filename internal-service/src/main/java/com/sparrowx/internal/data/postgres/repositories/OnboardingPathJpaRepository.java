2cd2e38f5905596b30f911585efdf9ad6e226090
Aggrey Lelei <aggreykiplimo@gmail.com>
2026-06-11 19:17:38 +0300

    chore(internal): minor refactoring

--- internal-service/src/main/java/com/sparrowx/internal/data/postgres/repositories/OnboardingPathJpaRepository.java
+++ internal-service/src/main/java/com/sparrowx/internal/data/postgres/repositories/OnboardingPathJpaRepository.java
@@ -1,11 +1,12 @@
 package com.sparrowx.internal.data.postgres.repositories;
 
 import com.sparrowx.internal.data.postgres.entities.OnboardingPathEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface OnboardingPathJpaRepository
         extends JpaRepository<OnboardingPathEntity, String> {
 
